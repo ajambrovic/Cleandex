620cd49a00e97a8eea847d521b2960468cf55293
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2018-08-23 18:21:03 +0200

    Initial filter implementation - done

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,48 @@
-chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
+chrome.extension.sendMessage({}, function (response) {
+	const readyStateCheckInterval = setInterval(function () {
+		if (document.readyState === "complete") {
+			clearInterval(readyStateCheckInterval);
 
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
+			const articles = document.querySelectorAll('li.clearfix');
+			const keywords = [
+				"Kardashian",
+				"Maja Šuput",
+				"FIFA",
+				"izazovne fotke",
+				"nevjerojatan trbuh",
+				"udala se",
+				"seksi snimkom",
+				"Meghan Markle",
+				"Franka Batelić",
+				"Severin",
+				"Igor Kojić",
+				"u vezi s duhom",
+				"Ana Gruica",
+				"Tulumarila na Zrću",
+				"Seksi",
+				"Modni mačak",
+				"Neven Ciganović",
+				"Jelena Veljača",
+				"Seks skandal",
+				"Ella Dvornik",
+				"Nikolina Ristović",
+				"gole grudi",
+			]
+			// get all article that contain a keyword
 
-	}
+			const filter = Array.prototype.filter;
+			const filteredArticles = filter.call(articles, function (article) {
+				const articleTitle = article.querySelector('.title');
+				const articleSummary = article.querySelector('.summary');
+				return keywords.some(s => {
+					return articleTitle.innerText.indexOf(s) > -1 || articleSummary.innerText.indexOf(s) > -1;
+				});
+			});
+			filteredArticles.forEach(filteredArticle => {
+				filteredArticle.style.display = 'none';
+				console.log(filteredArticle.querySelector('.title').innerText);
+				console.log(filteredArticle.querySelector('.summary').innerText);
+			});
+		}
 	}, 10);
 });
