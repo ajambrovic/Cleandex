cd4710354a5d21c8a0a1ed17cab1286115df1e85
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2018-08-24 17:11:46 +0200

    Outputting statistics instead of article details

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -12,13 +12,15 @@ function hideContentWithKeywords(keywords) {
 	const articles = Array.from(document.querySelectorAll('li.clearfix'));
 	// get all articles that contain a keyword
 	// and remove each one
+	let filteredArticlesCount = 0;
 	articles.filter(article => {
 		return filterArticlesWithKeywords(article, keywords);
 	}).forEach(filteredArticle => {
+		filteredArticlesCount++;
 		filteredArticle.style.display = 'none';
-		console.log(filteredArticle.querySelector('.title').innerText);
-		console.log(filteredArticle.querySelector('.summary').innerText);
+		// console.log("Naslov: " + filteredArticle.querySelector('.title').innerText + ", sažetak: " + filteredArticle.querySelector('.summary').innerText);
 	});
+	console.log("Ukupno članaka: " + articles.length + ", od toga filtriranih : " + filteredArticlesCount);
 }
 
 chrome.extension.sendMessage({}, function (response) {
