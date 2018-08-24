48c5e9fc7a19a70ae46810fca7cfd4345aba4a13
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2018-08-24 17:31:46 +0200

    Switched the icon to open the target page
    Instead of using the hack with homepage_url, switched to browserAction.onClicked and removed the unnecessary page_action.html

--- src/bg/background.js
+++ src/bg/background.js
@@ -11,3 +11,8 @@ chrome.extension.onMessage.addListener(
     chrome.pageAction.show(sender.tab.id);
     sendResponse();
   });
+
+chrome.browserAction.onClicked.addListener(function (activeTab) {
+  var newURL = "https://www.index.hr/najnovije/";
+  chrome.tabs.create({ url: newURL });
+});
