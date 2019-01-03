97aef2d46c5016f0f91afa6b9c4342fb7c500337
Andrija Jambrovic <andrija.jambrovic@solversint.com>
2019-01-03 20:18:39 +0100

    Removed unnecessary code

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,10 +1,3 @@
-//example of using a message handler from the inject scripts
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-    chrome.pageAction.show(sender.tab.id);
-    sendResponse();
-  });
-
 chrome.browserAction.onClicked.addListener(function (activeTab) {
   var newURL = "https://www.index.hr/najnovije/";
   chrome.tabs.create({ url: newURL });
